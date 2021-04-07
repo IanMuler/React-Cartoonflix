0009530431a4ad94afccb66675e4fcb618b53258
Ian Muler <Yoelmuler@gmail.com>
2021-04-07 01:20:27 -0300

    dropdown & carousel

--- src/components/Home_carousels.jsx
+++ src/components/Home_carousels.jsx
@@ -7,10 +7,14 @@ function Home_carousels(props) {
 
     const seriesCn = props.media.series.cn;
     const seriesDisney = props.media.series.disney;
+    const seriesFoxKids = props.media.series.foxkids;
+
+
 return (
     <Fragment>
         <Carousel series={seriesCn} classPic="cn" title="Cartoon Network" top="true"/>
         <Carousel series={seriesDisney} classPic="disney" title="Disney"/>
+        <Carousel series={seriesFoxKids} classPic="fox-kids" title="Fox Kids / Jetix"/>
     </Fragment>
 )
 }
